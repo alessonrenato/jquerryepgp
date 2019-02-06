fd481bd316e943662274bcc611753b7edd77769f
Alesson Renato Lopes Valenca <alessonrenato@gmail.com>
2019-02-06 17:18:11 -0300

    encerramento do projeto

---
+++ README.mk
@@ -0,0 +1,11 @@
+#THE REPUBLIC EPGP
+
+##Projeto parado devido ao encerramento da guild.
+
+Mantido apenas por motivo de pesquisa e contribuição. Caso deseje saber como funciona, confira o
+arquivo CONFIG.mk
+
+Agradecimentos a:
+	* http://Unrepentant.github.com/epgp/ (Criador do pluggin)
+	* http://code.google.com/p/epgp/ (Addon EPGP)
+	* Todos os membros da The Republic.
