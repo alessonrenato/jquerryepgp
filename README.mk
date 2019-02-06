5628a8a3a29603d648103a2926b15264b5df1168
Alesson Renato Lopes Valenca <alessonrenato@gmail.com>
2019-02-06 18:53:14 -0300

    english readme

--- README.mk
+++ README.mk
@@ -1,11 +1,20 @@
-#THE REPUBLIC EPGP
+# THE REPUBLIC EPGP
 
-##Projeto parado devido ao encerramento da guild.
+## Projeto parado devido ao encerramento da guild.
 
 Mantido apenas por motivo de pesquisa e contribuição. Caso deseje saber como funciona, confira o
 arquivo CONFIG.mk
+
+Pluggin para formatar database do addon EPGP em página WEB.
 
 Agradecimentos a:
 	* http://Unrepentant.github.com/epgp/ (Criador do pluggin)
 	* http://code.google.com/p/epgp/ (Addon EPGP)
 	* Todos os membros da The Republic.
+
+--English
+
+Deprecated project. Only for contribution and research. 
+If you do know about configuration, please check the CONFIG.mk file.
+Pluggin to format EPGP addon database in a webpage.
+
